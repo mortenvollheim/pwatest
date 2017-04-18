ce70a74c7804b54e92d26ea83c23c9fdadd0788e
Morten Vollheim <mortenv@flytit.no>
2017-04-18 11:11:27 +0200

    Files

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Install the service worker.
+this.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open('v1').then(function(cache) {
+			// The cache will fail if any of these resources can't be saved.
+			return cache.addAll([
+				// Path is relative to the origin, not the app directory.
+				'/pwa-photobooth/',
+				'/pwa-photobooth/index.html',
+				'/pwa-photobooth/css/styles.css',
+				'/pwa-photobooth/fonts/MaterialIcons-Regular.woff2',
+				'/pwa-photobooth/js/script.js',
+				'/pwa-photobooth/icons/ic-face.png',
+				'/pwa-photobooth/icons/ic-face-large.png',
+				'/pwa-photobooth/manifest.json'
+			])
+			.then(function() {
+				console.log('Success! App is available offline!');
+			})
+		})
+	);
+});
+
+
+// Define what happens when a resource is requested.
+// For our app we do a Cache-first approach.
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+	    // Try the cache.
+	    caches.match(event.request)
+    	.then(function(response) {
+			// Fallback to network if resource not stored in cache.
+			return response || fetch(event.request);
+		})
+  	);
+});
